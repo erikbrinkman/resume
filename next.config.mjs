612d2de47cf6995e0caf615bb2aeac2b423eb698
Erik Brinkman <erik.brinkman@gmail.com>
2023-07-11 23:17:29 -0400

    upgrade deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
 export const reactStrictMode = true;
 export const assetPrefix =
   process.env.NODE_ENV === "production" ? "/resume" : undefined;
+export const output = "export";
+export const distDir =
+  process.env.NODE_ENV === "production" ? "docs" : undefined;
