f511677c74549f1d64c06bd4d28fb92692012c1c
Erik Brinkman <erik.brinkman@gmail.com>
2023-10-06 22:15:34 -0400

    upgrade and release

--- docs/_next/static/chunks/app/layout-2aa4dad385153007.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9814:function(n,e,u){Promise.resolve().then(u.t.bind(u,4435,23))},4435:function(){}},function(n){n.O(0,[143,386,744],function(){return n(n.s=9814)}),_N_E=n.O()}]);
---
+++ docs/_next/static/chunks/app/layout-661b51251d987e07.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9944:function(n,e,u){Promise.resolve().then(u.t.bind(u,389,23))},389:function(){}},function(n){n.O(0,[750,664,744],function(){return n(n.s=9944)}),_N_E=n.O()}]);
