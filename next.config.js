8918c20c262c2c49817b0e9ca9cd6cd73e9609df
Erik Brinkman <erik.brinkman@gmail.com>
2024-07-22 22:08:19 -0400

    fix next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ export default {
   reactStrictMode: true,
   assetPrefix: process.env.NODE_ENV === "production" ? "/resume" : undefined,
   output: "export",
-  distDir: process.env.NODE_ENV === "production" ? "docs" : undefined,
   images: {
     unoptimized: true,
   },
